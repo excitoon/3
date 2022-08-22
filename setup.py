e28cd5ae19c1c09694548224a7e659d8e524717a
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2022-08-23 02:20:05 +0300

    Blackified scripts.

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ import setuptools
 
 
 setuptools.setup(
-    name='3-py',
-    version='0.0.2',
-    author='Vladimir Chebotarev',
-    description='Tree tool written in Python',
+    name="3-py",
+    version="0.0.2",
+    author="Vladimir Chebotarev",
+    description="Tree tool written in Python",
     packages=[],
-    scripts=['3'],
-    install_requires=[]
+    scripts=["3"],
+    install_requires=[],
 )
