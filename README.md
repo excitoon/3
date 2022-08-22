ef9465862db60fdb2b64a8ebc8d1f3f6a475e3b5
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2022-08-22 16:13:32 +0300

    Initial.

---
+++ README.md
@@ -0,0 +1,15 @@
+Tree tool written in Python.
+
+Example:
+
+```
+3
+```
+
+```
+.gitignore
+3
+LICENSE.md
+README.md
+setup.py
+```
---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+
+setuptools.setup(
+    name='3-py',
+    version='0.0.0',
+    author='Vladimir Chebotarev',
+    description='Tree tool written in Python',
+    packages=[],
+    install_requires=[]
+)
