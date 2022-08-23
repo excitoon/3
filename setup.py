105fd3bddc9da3f783985b1f662968eaf2e48486
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2022-08-23 03:49:08 +0300

    Added statistics.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="3-py",
-    version="1.0.0",
+    version="1.0.1",
     author="Vladimir Chebotarev",
     description="`.gitignore`-aware tree tool written in Python",
     packages=[],
