29a0dc84f604cfc7d008f9d369acc8fd9ffdb0db
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2022-08-23 13:48:58 +0300

    Proper handling of parent directories.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="3-py",
-    version="1.0.4",
+    version="1.0.5",
     description="`.gitignore`-aware tree tool written in Python",
     long_description="`.gitignore`-aware tree tool written in Python.",
     long_description_content_type="text/markdown",
