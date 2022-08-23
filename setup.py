a44d9f8c38de6cf2978c8bc213cc7ada45273159
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2022-08-23 11:33:56 +0300

    Better project metadata.

--- setup.py
+++ setup.py
@@ -3,9 +3,24 @@ import setuptools
 
 setuptools.setup(
     name="3-py",
-    version="1.0.2",
-    author="Vladimir Chebotarev",
+    version="1.0.3",
     description="`.gitignore`-aware tree tool written in Python",
+    long_description="`.gitignore`-aware tree tool written in Python.",
+    long_description_content_type="text/markdown",
+    author="Vladimir Chebotarev",
+    author_email="vladimir.chebotarev@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+    ],
+    keywords="git gitignore tree",
+    project_urls={
+        "Documentation": "https://github.com/excitoon/3/blob/master/README.md",
+        "Source": "https://github.com/excitoon/3",
+        "Tracker": "https://github.com/excitoon/3/issues",
+    },
     packages=[],
     scripts=["3"],
     install_requires=["gitignore-parser"],
