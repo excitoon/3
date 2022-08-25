65d30736ef6745d09ddaf451c25b5852ad18b558
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2022-08-25 11:04:34 +0300

    Don't call `stat` twice.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="3-py",
-    version="1.1.1",
+    version="1.1.2",
     description="`.gitignore`-aware tree tool written in Python",
     long_description="`.gitignore`-aware tree tool written in Python.",
     long_description_content_type="text/markdown",
