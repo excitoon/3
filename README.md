a972f4176c6b8b0053f725358bdb3779ca82f878
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2022-08-27 23:11:45 +0300

    Added tests.

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # `3`
 
+[![Black](https://github.com/excitoon/3/actions/workflows/black.yml/badge.svg)](https://github.com/excitoon/3/actions/workflows/black.yml)
+[![Linux](https://github.com/excitoon/3/actions/workflows/ubuntu.yml/badge.svg)](https://github.com/excitoon/3/actions/workflows/ubuntu.yml)
+[![OS X](https://github.com/excitoon/3/actions/workflows/macos.yml/badge.svg)](https://github.com/excitoon/3/actions/workflows/macos.yml)
+[![Windows](https://github.com/excitoon/3/actions/workflows/windows.yml/badge.svg)](https://github.com/excitoon/3/actions/workflows/windows.yml)
+[![PyPI](https://badge.fury.io/py/3-py.svg)](https://badge.fury.io/py/3-py)
+
 `.gitignore`-aware tree tool written in Python.
 
 Example:
---
+++ tests.py
@@ -0,0 +1,42 @@
+import os
+import subprocess
+import unittest
+import unittest.mock
+
+
+class Test3(unittest.TestCase):
+    def test_simple(self):
+        env = {k: v for k, v in os.environ.items() if not k.endswith("_COLORS")}
+        env["PYTHONUTF8"] = "1"
+        process = subprocess.run(
+            os.path.abspath("3"),
+            shell=True,
+            env=env,
+            stdout=subprocess.PIPE,
+        )
+        self.assertEqual(process.returncode, 0)
+        self.assertEqual(
+            process.stdout,
+            """
+[01;34m.[0m
+├── [01;34m.github[0m
+│   └── [01;34mworkflows[0m
+│       ├── [00mblack.yml[0m
+│       ├── [00mmacos.yml[0m
+│       ├── [00mubuntu.yml[0m
+│       └── [00mwindows.yml[0m
+├── [00m.gitignore[0m
+├── [01;32m3[0m
+├── [01;32m3.cmd[0m
+├── [01;34mexamples[0m
+│   └── [00m3.png[0m
+├── [00mLICENSE.md[0m
+├── [00mpyproject.toml[0m
+├── [00mREADME.md[0m
+├── [00mrequirements.txt[0m
+├── [00msetup.py[0m
+└── [00mtests.py[0m
+
+3 directories, 14 files
+""".lstrip().encode(),
+        )
